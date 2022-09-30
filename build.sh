f63c6988fabce094aa3f0c93212384fd3e548141
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-09-30 15:58:34 +0530

    update build.sh with fixes

--- build.sh
+++ build.sh
@@ -5,11 +5,16 @@ test -d "themes/PaperMod/.git" || (git submodule init && git submodule update)
 if [[ ! -f "$APP_ROOT/bin/hugo" ]]
 then
     DOWNLOAD_DIR="$TMPDIR/hugo"
-    mkdir "$DOWNLOAD_DIR"
-    curl -L -o "$DOWNLOAD_DIR/hugo.tar.gz" https://github.com/gohugoio/hugo/releases/download/v0.104.2/hugo_0.104.2_linux-amd64.tar.gz
-    tar -C "$APP_ROOT/bin" -xvzf "$DOWNLOAD_DIR/hugo.tar.gz" hugo
+    mkdir "$DOWNLOAD_DIR" && \
+    curl -L -o "$DOWNLOAD_DIR/hugo.tar.gz" https://github.com/gohugoio/hugo/releases/download/v0.104.2/hugo_0.104.2_linux-amd64.tar.gz && \
+    tar -C "$APP_ROOT/bin" -xvzf "$DOWNLOAD_DIR/hugo.tar.gz" hugo && \
     rm -r "$DOWNLOAD_DIR"
+
+    if [[ $? -ne 0 ]]; then
+        echo "something went wrong while installing hugo"
+        exit 1
+    fi
 fi
 
 # build static site with hugo
-hugo -b kaustubh.kaustubh-dev.pipal.in -d "$APP_ROOT/site"
+rm -r "$APP_ROOT/site/*" && hugo -b http://kaustubh.kaustubh-dev.pipal.in/ -d "$APP_ROOT/site"
